d1bd0e806a6d889049f31bd04991ffc7bf875467
Sushil Thapa <User@LAPTOP-PFEVT2F2>
2016-08-02 09:33:00 +1200

    latest change

--- hospitalRegistration/src/hospitalRegistration/loginForm.java
+++ hospitalRegistration/src/hospitalRegistration/loginForm.java
@@ -38,7 +38,7 @@ public class loginForm {
 				}
 			}
 		});
-		}
+	}
 
 	/**
 	 * Create the application.
@@ -55,58 +55,59 @@ public class loginForm {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setBounds(195, 11, 86, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblUserName = new JLabel("User Name");
 		lblUserName.setBounds(93, 17, 92, 14);
 		frame.getContentPane().add(lblUserName);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(195, 42, 87, 20);
 		frame.getContentPane().add(passwordField);
-		
+
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(93, 45, 67, 14);
 		frame.getContentPane().add(lblPassword);
-		
+
 		final JLabel error = new JLabel("Error : Invalid user name or password");
 		error.setVisible(false);
 		error.setForeground(Color.RED);
 		error.setBounds(93, 73, 232, 14);
 		frame.getContentPane().add(error);
-		
+
 		JButton btnLogIn = new JButton("Log In");
 		btnLogIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				String userName = textField.getText();
 				String password = new String(passwordField.getPassword());
 				Connection dbCon = new databaseConnection().getConnection();
-				try {					
-					String query = "SELECT * FROM USER WHERE userName='"+userName+"' and password='"+password+"'";
+				try {
+					String query = "SELECT * FROM USER WHERE userName='" + userName + "' and password='" + password
+							+ "'";
 					Statement stat = (Statement) dbCon.createStatement();
 					ResultSet result = stat.executeQuery(query);
-					if(result.first()){
-						//while(result.next()){
-							frame.dispose();
-							try {
-								registrationForm windowReg = new registrationForm();
-								windowReg.frame.setVisible(true);
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						//}	
-					}else{
+					if (result.first()) {
+						// while(result.next()){
+						frame.dispose();
+						try {
+							registrationForm windowReg = new registrationForm();
+							windowReg.frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						// }
+					} else {
 						error.setText("Error : Invalid user name or password");
 						error.setVisible(true);
 					}
-					
+
 				} catch (Exception e) {
 					// TODO: handle exception
-					//System.out.println(e);
+					// System.out.println(e);
 					error.setText("Please Check Server");
 					error.setVisible(true);
 				}
