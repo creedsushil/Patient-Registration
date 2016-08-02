f9b980fae62fe641c50d31777bf7caf7581f98bf
Sushil Thapa <User@LAPTOP-PFEVT2F2>
2016-08-03 10:10:30 +1200

    Remove commit

--- hospitalRegistration/src/hospitalRegistration/databaseConnection.java
+++ hospitalRegistration/src/hospitalRegistration/databaseConnection.java
@@ -9,21 +9,6 @@ import com.mysql.jdbc.Statement;
 
 public class databaseConnection {
 public static void main(String[] args) throws SQLException{
-	/*Connection conn = null;
-	try {
-		conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/patientReg", "root", "");
-		//System.out.println("Connected");
-		String query = "SELECT * FROM USER";
-		Statement stat = (Statement) conn.createStatement();
-		ResultSet result = stat.executeQuery(query);
-		while(result.next()){
-			System.out.println("userName :"+result.getString("userName"));
-		}
-	} catch (Exception e) {
-		// TODO: handle exception
-		System.err.println(e);
-	}*/
-	//conn.close();
 }
 
 public Connection getConnection(){
